b57c94d6fb9427159a8ccff5773036fecc69763b
codes4coffee <jakelanc950@gmail.com>
2019-01-14 18:25:51 -0600

    Changed API to tally up spots per permit into one field

--- main.py
+++ main.py
@@ -15,28 +15,29 @@ else:
 parkingStructures = ['Parking Structure 1', 'Parking Structure 3', 'Parking Structure 4']
 
 def getParkingSpaces(request):
-    final_list = []
-    # List the amount of spots for every parking structure 1 - 4
+    finalList = []
+    #List the amount of spots for every parking structure 1 - 4
     for structure in parkingStructures:
         jsonObj = {'structure': structure}
+        jsonObj['green'] = 0;
+        jsonObj['gold'] = 0;
+        jsonObj['orange'] = 0;
+        jsonObj['purple'] = 0;
+        jsonObj['pay_by_space'] = 0;
         parkingTable = soup.find('table', attrs={'summary':structure})
-        tableData = parkingTable.tbody  # the sub-table for a single parking structure
+        tableData = parkingTable.tbody # the sub-table for a single parking structure
         for child in tableData:
-            if hasattr(child, 'attrs'):
+            if hasattr(child,'attrs'):
 
-                # Get the permit type of the space and strip out the type from the html tag
-                color = str(child.td.next_sibling.next_sibling)[11:]
-                print(color)
-                level = str(child.td)[19:]
+                color = str(child.td.next_sibling.next_sibling)[11:] #Get the permit type of the space and strip out the type from the html tag
                 color = color[:color.find('"')]
-                level = level[:level.find('<')]
-                color = color + '-' + level
-
+                color = color[8:]
                 numSpace = str(child.td.next_sibling.next_sibling.next_sibling.next_sibling.contents[0])
                 if numSpace[0] == '<':
-                    jsonObj[color] = 0
+                    print("Test")
                 else:
-                    jsonObj[color] = int(numSpace)
-        final_list.append(jsonObj)
-        resp = Response(json.dumps(final_list), mimetype='application/json')
+                    print(color + "-" + numSpace)
+                    jsonObj[color] = jsonObj[color] + int(numSpace)
+        finalList.append(jsonObj)
+    resp = Response(json.dumps(finalList), mimetype='application/json')
     return resp
--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@ import pytest
 import main
 import hashlib
 
-master_hash = hashlib.md5(str.encode('[{"structure": "Parking Structure 1", "parking_green-5": 0, "parking_gold-4": 155, "parking_gold-3": 112, "parking_orange-3": 41, "parking_orange-2": 114, "parking_purple-2": 41, "parking_pay_by_space-1": 91}, {"structure": "Parking Structure 3", "parking_green-5": 0, "parking_gold-4": 166, "parking_gold-3": 102, "parking_orange-3": 65, "parking_orange-2": 108, "parking_purple-1": 32, "parking_pay_by_space-1": 33}, {"structure": "Parking Structure 4", "parking_green-5": 0, "parking_gold-4": 243, "parking_gold-3": 235, "parking_orange-2": 198, "parking_purple-2": 28, "parking_purple-1": 10, "parking_pay_by_space-1": 136}]'))
+master_hash = hashlib.md5(str.encode('[{"structure": "Parking Structure 1", "green": 0, "gold": 267, "orange": 155, "purple": 41, "pay_by_space": 91}, {"structure": "Parking Structure 3", "green": 0, "gold": 268, "orange": 173, "purple": 32, "pay_by_space": 33}, {"structure": "Parking Structure 4", "green": 0, "gold": 478, "orange": 198, "purple": 38, "pay_by_space": 136}]'))
 
 def test_getParkingSpaces():
     resp = main.getParkingSpaces("Test")
