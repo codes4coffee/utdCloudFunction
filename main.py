fe37064f7f825e7c002c7ddb8812439276517a56
codes4coffee <jakelanc950@gmail.com>
2019-01-19 18:34:32 -0600

    Added code to check if garage information is unvailable and report it

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask import Response
 import json
 import os
 
-print (os.environ['TESTING'])
 if os.environ['TESTING'] == 'true':
     soup = BeautifulSoup(open('testPage.html'), 'html.parser')
 else:
@@ -16,27 +15,29 @@ parkingStructures = ['Parking Structure 1', 'Parking Structure 3', 'Parking Stru
 
 def getParkingSpaces(request):
     finalList = []
-    #List the amount of spots for every parking structure 1 - 4
+    # List the amount of spots for every parking structure 1 - 4
     for structure in parkingStructures:
         jsonObj = {'structure': structure}
+        jsonObj['hasInfo'] = True; # Sometimes garage isn't available. Consumers need to know this
         jsonObj['green'] = 0;
         jsonObj['gold'] = 0;
         jsonObj['orange'] = 0;
         jsonObj['purple'] = 0;
         jsonObj['pay_by_space'] = 0;
         parkingTable = soup.find('table', attrs={'summary':structure})
-        tableData = parkingTable.tbody # the sub-table for a single parking structure
+
+        if parkingTable is None:
+            jsonObj['hasInfo'] = False # Needed to add this in because sometimes all the garage info isn't available
+            continue;
+
+        tableData = parkingTable.tbody # The sub-table for a single parking structure
         for child in tableData:
             if hasattr(child,'attrs'):
-
-                color = str(child.td.next_sibling.next_sibling)[11:] #Get the permit type of the space and strip out the type from the html tag
+                color = str(child.td.next_sibling.next_sibling)[11:] # Get the permit type of the space and strip out the type from the html tag
                 color = color[:color.find('"')]
                 color = color[8:]
                 numSpace = str(child.td.next_sibling.next_sibling.next_sibling.next_sibling.contents[0])
-                if numSpace[0] == '<':
-                    print("Test")
-                else:
-                    print(color + "-" + numSpace)
+                if numSpace[0] != '<':
                     jsonObj[color] = jsonObj[color] + int(numSpace)
         finalList.append(jsonObj)
     resp = Response(json.dumps(finalList), mimetype='application/json')
--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@ import pytest
 import main
 import hashlib
 
-master_hash = hashlib.md5(str.encode('[{"structure": "Parking Structure 1", "green": 0, "gold": 267, "orange": 155, "purple": 41, "pay_by_space": 91}, {"structure": "Parking Structure 3", "green": 0, "gold": 268, "orange": 173, "purple": 32, "pay_by_space": 33}, {"structure": "Parking Structure 4", "green": 0, "gold": 478, "orange": 198, "purple": 38, "pay_by_space": 136}]'))
+master_hash = hashlib.md5(str.encode('[{"structure": "Parking Structure 1", "hasInfo": true, "green": 0, "gold": 267, "orange": 155, "purple": 41, "pay_by_space": 91}, {"structure": "Parking Structure 3", "hasInfo": true, "green": 0, "gold": 268, "orange": 173, "purple": 32, "pay_by_space": 33}, {"structure": "Parking Structure 4", "hasInfo": true, "green": 0, "gold": 478, "orange": 198, "purple": 38, "pay_by_space": 136}]'))
 
 def test_getParkingSpaces():
     resp = main.getParkingSpaces("Test")
