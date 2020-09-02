a537cbf89a20467ad579e0d57a508457a1d88642
codes4coffee <jakelanc950@gmail.com>
2020-09-01 21:39:44 -0500

    Changed parking scraper to get full names from email

--- main.py
+++ main.py
@@ -3,43 +3,30 @@ from bs4 import BeautifulSoup
 from flask import Response
 import json
 import os
+import unicodedata
 
-if os.environ['TESTING'] == 'true':
-    soup = BeautifulSoup(open('testPage.html'), 'html.parser')
-else:
-    page = urlopen(Request('https://www.utdallas.edu/services/transit/garages/_code.php',
-                           headers={'User-Agent': 'Mozilla'}))
+
+def remove_control_characters(s):
+    return "".join(ch for ch in s if unicodedata.category(ch)[0]!="C")
+
+def get_name_from_email(email):
+    page = urlopen(Request('https://www.utdallas.edu/directory/includes/directories.class.php?dirType=displayname&dirSearch='+email+'&dirAffil=All&dirDept=All&dirMajor=All&dirSchool=All',
+                        headers={'User-Agent': 'Mozilla'}))
+    print(page)
     soup = BeautifulSoup(page, 'html.parser')
+    name = soup.find('h3')
+    print(name)
+    if name is None:
+        name = ''
+    else:
+        name = name.get_text()
+    return name
 
-parkingStructures = ['Parking Structure 1', 'Parking Structure 3', 'Parking Structure 4']
-
-def getParkingSpaces(request):
-    finalList = []
-    # List the amount of spots for every parking structure 1 - 4
-    for structure in parkingStructures:
-        jsonObj = {'structure': structure}
-        jsonObj['hasInfo'] = True; # Sometimes garage isn't available. Consumers need to know this
-        jsonObj['green'] = 0;
-        jsonObj['gold'] = 0;
-        jsonObj['orange'] = 0;
-        jsonObj['purple'] = 0;
-        jsonObj['pay_by_space'] = 0;
-        parkingTable = soup.find('table', attrs={'summary':structure})
-
-        if parkingTable is None:
-            jsonObj['hasInfo'] = False # Needed to add this in because sometimes all the garage info isn't available
-            finalList.append(jsonObj)
-            continue;
-
-        tableData = parkingTable.tbody # The sub-table for a single parking structure
-        for child in tableData:
-            if hasattr(child,'attrs'):
-                color = str(child.td.next_sibling.next_sibling)[11:] # Get the permit type of the space and strip out the type from the html tag
-                color = color[:color.find('"')]
-                color = color[8:]
-                numSpace = str(child.td.next_sibling.next_sibling.next_sibling.next_sibling.contents[0])
-                if numSpace[0] != '<':
-                    jsonObj[color] = jsonObj[color] + int(numSpace)
-        finalList.append(jsonObj)
-    resp = Response(json.dumps(finalList), mimetype='application/json')
-    return resp
+outFile = open('emails-w-names.csv', 'w')
+outFile.write('email, name\n')
+with open('subscriber-emails.txt', 'r') as file:
+    for line in file:
+        outLine = remove_control_characters(line) + ',"' + get_name_from_email(remove_control_characters(line)) + '"\n'
+        outFile.write(outLine)
+outFile.close()
+print("Finished :)")
